6d821fd7a247d35cc6a184d0af3a6761e36f9e56
Ninjseal <b.marius10@yahoo.com>
2020-06-11 01:28:02 +0300

    Quick fix for Back Button Management Script.

--- Assets/StartMenu/Scripts/BackButtonManagement.cs
+++ Assets/StartMenu/Scripts/BackButtonManagement.cs
@@ -14,7 +14,7 @@ public class BackButtonManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
             SceneManager.LoadSceneAsync("start");
         }

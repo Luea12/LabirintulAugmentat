7282091972ad95eede4537ff03220ff0f00eb614
Alexandru Balan <balan.alexandru1997@tutanota.com>
2020-06-11 17:53:36 +0300

    Coins are now read from a json file upon game start

---
+++ Assets/ShopLogic/CoinLogic.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class CoinLogic : MonoBehaviour
+{
+    private Shop shop;
+    private TextMeshProUGUI coinsValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readShop();
+
+        coinsValue = GameObject.Find("CoinsValue").GetComponent<TextMeshProUGUI>();
+        coinsValue.text = shop.coins.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void readShop()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/ShopLogic/Shop.json");
+        shop = JsonConvert.DeserializeObject<Shop>(json);
+    }
+
+    class Shop
+    {
+        public int coins { get; set; }
+    }
+}

b4ad638c2fb4fb6d709cbf067aa881ebe920c563
Alexandru Balan <balan.alexandru1997@tutanota.com>
2020-06-11 21:34:33 +0300

    The slots for available and owned characters can now be dynamically added to their respective panels.
    The shop class has two lists retaining those characters.

--- Assets/ShopLogic/CoinLogic.cs
+++ Assets/ShopLogic/CoinLogic.cs
@@ -1,31 +1,48 @@
 ﻿using Newtonsoft.Json;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinLogic : MonoBehaviour
 {
     private Shop shop;
     private TextMeshProUGUI coinsValue;
+    public GameObject availableCharacterPrefab;
+    public GameObject ownedCharacterPrefab;
+    public ScrollRect AvailablePanel;
+    public ScrollRect OwnedPanel;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Read the shop data
         readShop();
-
         coinsValue = GameObject.Find("CoinsValue").GetComponent<TextMeshProUGUI>();
         coinsValue.text = shop.coins.ToString();
+
+        // Instantiate the characters that are available for purchase
+        // Call this as many times as necessary
+        CreateAvailableSlot(new Vector3(0, 50, 0));
+        CreateAvailableSlot(new Vector3(0, 250, 0));
+
+        // Instantiate the characters that are owned by the user
+        // Call this as many times as necessary
+        CreateOwnedSlot(new Vector3(0, 50, 0));
+        CreateOwnedSlot(new Vector3(0, 250, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        coinsValue.text = shop.coins.ToString();
     }
 
-
+    public void consumeCoins(int coins)
+    {
+        this.shop.coins -= coins;
+    }
 
     void readShop()
     {
@@ -33,8 +50,24 @@ public class CoinLogic : MonoBehaviour
         shop = JsonConvert.DeserializeObject<Shop>(json);
     }
 
+    void CreateAvailableSlot(Vector3 position)
+    {
+        GameObject availableSlot = Instantiate(availableCharacterPrefab, position, Quaternion.identity);
+        availableSlot.transform.SetParent(AvailablePanel.content.transform, false);
+        shop.availableCharacters.Add(availableSlot);
+    }
+
+    void CreateOwnedSlot(Vector3 position)
+    {
+        GameObject ownedSlot = Instantiate(ownedCharacterPrefab, position, Quaternion.identity);
+        ownedSlot.transform.SetParent(OwnedPanel.content.transform, false);
+        shop.ownedCharacters.Add(ownedSlot);
+    }
+
     class Shop
     {
         public int coins { get; set; }
+        public List<GameObject> ownedCharacters { get; set; }
+        public List<GameObject> availableCharacters { get; set; }
     }
 }

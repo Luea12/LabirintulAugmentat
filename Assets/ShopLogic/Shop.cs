d153096c605e4b0c80da32e14206d5e663caafcc
Ninjseal <b.marius10@yahoo.com>
2020-06-13 02:22:46 +0300

    Added persistence to Shop. Fix Shop Scene problems.

--- Assets/ShopLogic/Shop.cs
+++ Assets/ShopLogic/Shop.cs
@@ -2,21 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shop : MonoBehaviour
+public class Shop
 {
     public int coins { get; set; }
     public List<GameObject> ownedCharacters { get; set; } = new List<GameObject>();
     public List<GameObject> availableCharacters { get; set; } = new List<GameObject>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
